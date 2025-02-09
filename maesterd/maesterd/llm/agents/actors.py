275b3c625f14993855556e65f9b24ee357fdc604
mustafaeskin <mustafasamedeskin@gmail.com>
2025-02-09 18:14:23 +0100

    add update with actor prompts to actor node

--- maesterd/maesterd/llm/agents/actors.py
+++ maesterd/maesterd/llm/agents/actors.py
@@ -1,16 +1,15 @@
-import click
 from typing import Literal, TypedDict
 
 from langchain_core.messages import HumanMessage
 from langgraph.types import Command
+from langgraph.types import interrupt
 
 
 NAME = "actors"
 
 
-def node(state: TypedDict) -> Command[Literal["router"]]:
-    r = click.prompt("What do you do? ")
+def node(state: TypedDict) -> Command[Literal["router"]]:  # noqa
     return Command(
-        update={"messages": [HumanMessage(content=r, name=NAME)]},
+        update={"messages": [HumanMessage(content=state['actor_prompts'][-1], name=NAME)]},
         goto="router",
     )
