6a047e732c0f1bcd472b17f9878741dd5878d0c2
uncletoxa <7903257+uncletoxa@users.noreply.github.com>
2025-02-21 18:19:35 +0100

    feat: update socket requests function

--- maesterd_web/settings.py
+++ maesterd_web/settings.py
@@ -5,4 +5,4 @@ env.read_env()
 MAX_TITLE_LENGTH = env.int('MAX_TITLE_LENGTH', default=140)
 STORIES_PER_PAGE = env.int('STORIES_PER_PAGE', default=20)
 CHAPTERS_PER_PAGE = env.int('CHAPTERS_PER_PAGE', default=1000)  # I have no idea how to do a pagination with prompt
-OPENAI_SOCKET_ADDR = env.str('OPENAI_SOCKET_ADDR', '/tmp/sockets/openai_service.sock')
+OPENAI_SOCKET_ADDR = env.str('OPENAI_SOCKET_ADDR', '/tmp/sockets/maesterd.sock')
